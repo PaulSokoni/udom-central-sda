4069fdeb0dec93a0266d64376f2761ec9ab77877
Paul Sokoni <waltergodliving05@gmail.com>
2026-05-27 08:06:52 +0300

    Set ALLOWED_HOSTS=['*'] to fix 400 from Railway internal proxy host headers

--- backend/udom_central/settings.py
+++ backend/udom_central/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',') + ['healthcheck.railway.app']
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
